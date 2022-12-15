82329098520554535c49d1a61e44b6119ba3332d
Kyle Jensen <kjensen19@gmail.com>
2022-12-14 22:33:06 -0600

    test router GET route built

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -3,4 +3,22 @@ const router = express.Router()
 const axios = require('axios');
 
 
+router.get('/api/test', (req, res) => {
+    try{
+       const queryText = `
+        SELECT * from "test"
+            WHERE "id=$1;
+       `
+       const queryVal = ['1']
+       pool.query(queryText, queryVal)
+        .then((dbRes) =>{
+            res.send(dbRes.rows[0])
+        }
+        )
+    }catch(error){
+        console.log('Error in test GET', error)
+        res.sendStatus(500)
+    }
+})
+
 module.exports = router
