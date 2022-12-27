559d66b6e59b0144a2b905f9083fbad9e6e945b9
Kyle Jensen <kjensen19@gmail.com>
2022-12-26 23:25:24 -0600

    add server side post route skeleton

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -24,4 +24,24 @@ router.get('/', (req, res) => {
     }
 })
 
+router.post('/', (req, res) => {
+    console.log('in Post');
+    try{
+        const queryText = `
+        INSERT INTO "test"
+        ("name", "number", "time", "date")
+        VALUES($1, $2, $3, $4)
+        `
+
+        const queryVal = ['Kyle', '5', '7', '12/26/2022']
+        pool.query((dbRes) => {
+            console.log('Made it!')
+            res.sendStatus(200)
+        })
+    }catch(error){
+        console.log('Error in POST', error)
+        res.sendStatus(500)
+    }
+})
+
 module.exports = router
